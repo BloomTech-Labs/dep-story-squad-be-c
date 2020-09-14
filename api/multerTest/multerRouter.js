e0872991f5c775ac0c183ab1f13a37136f594eb4
John Daly <jcdaly97@gmail.com>
2020-09-14 15:49:46 -0700

    removed the aws keys

--- api/multerTest/multerRouter.js
+++ api/multerTest/multerRouter.js
@@ -7,8 +7,8 @@ const multerS3 = require('multer-s3'); //"^2.7.0"
 
 
 aws.config.update({
-    secretAccessKey: 'AKIASVA5GJL535UIVS4K',
-    accessKeyId: 'z9g4GV4yHhWzBp7QLOVvXX1Xsj3oWSBNnDGbnCAL',
+    secretAccessKey: 'secret key',
+    accessKeyId: 'key',
     region: 'us-west-2'
 });
 
