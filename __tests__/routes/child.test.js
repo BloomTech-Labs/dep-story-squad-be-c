ac991cc89887b1f1d0aa1c9ae6526ca5afa3154a
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-10-01 14:52:41 -0700

    added write and draw enpoints, tested, updated readme, added pull reaquest template

---
+++ __tests__/routes/child.test.js
@@ -0,0 +1,116 @@
+const request = require('supertest');
+const express = require('express');
+const Child = require('../../api/child/childModel');
+const childRouter = require('../../api/child/childRouter');
+const server = express();
+server.use(express.json());
+
+jest.mock('../../api/child/childModel');
+
+jest.mock('../../api/middleware/authRequired', () =>
+  jest.fn((req, res, next) => next())
+);
+
+jest.mock('../../api/middleware/jwtRestricted', () =>
+  jest.fn((req, res, next) => next())
+);
+
+describe('Test Suite', () => {
+  it('tests if jest is working', () => {
+    expect(3).toBe(3);
+  });
+});
+
+describe('Child router endpoints', () => {
+  beforeAll(() => {
+    server.use('/child', childRouter);
+    jest.clearAllMocks();
+  });
+
+  const child = {
+    id: 1,
+    name: 'Billy',
+    writing_score: 80,
+    avatar_url: null,
+    pin: '1234',
+    type: 'child',
+    username: 'BillyBob',
+    dyslexic: 0,
+    parent_id: 1,
+    current_mission: 1,
+  };
+
+  const mission = {
+    mission_id: 1,
+    read: 'hello',
+    write: 'test',
+    draw: 'test',
+  };
+
+  describe('POST /child/:id', () => {
+    it('should return 400 when nothing is sent', () => {
+      Child.findById.mockResolvedValue(child);
+      return request(server)
+        .post('/child/1')
+        .send({})
+        .then((res) => {
+          expect(res.status).toBe(400);
+        });
+    });
+
+    it('should return 400 if no child sent', () => {
+      Child.findById.mockResolvedValue(null);
+      return request(server)
+        .post('/child/1')
+        .send({ pin: '1234' })
+        .then((res) => {
+          expect(res.status).toBe(400);
+        });
+    });
+
+    it('should return 400 if wrong pin', () => {
+      Child.findById.mockResolvedValue(child);
+      return request(server)
+        .post('/child/1')
+        .send({ pin: '1235' })
+        .then((res) => {
+          expect(res.status).toBe(400);
+        });
+    });
+
+    it('should return 200', () => {
+      Child.findById.mockResolvedValue(child);
+      return request(server)
+        .post('/child/1')
+        .send({ pin: '1234' })
+        .then((res) => {
+          expect(res.status).toBe(200);
+        });
+    });
+  });
+
+  it('should allow multiple file uploads', () => {
+    Child.findById.mockResolvedValue(child);
+    Child.addWriting.mockResolvedValue({})
+    return request(server)
+    .post('/child/1/mission/write')
+    .attach('images', '__tests__/surprised.jpg')
+    .attach('images', '__tests__/surprised.jpg')
+    .then(res => {
+      expect(res.status).toBe(200)
+    })
+  })
+
+  it('should allow fingle file uploads', () => {
+    Child.findById.mockResolvedValue(child);
+    Child.addWriting.mockResolvedValue({})
+    return request(server)
+    .post('/child/1/mission/draw')
+    .attach('image', '__tests__/surprised.jpg')
+    .then(res => {
+      console.log(res)
+      expect(res.status).toBe(200)
+    })
+  })
+
+});
--- api/child/childRouter.js
+++ api/child/childRouter.js
@@ -5,183 +5,213 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const upload = require('../middleware/multer');
 const multiUpload = upload.array('images', 5);
+const singleUpload = upload.single('image');
 const checkToken = require('../middleware/jwtRestricted');
 
 //token creator for our JWT
 function createToken(user) {
-    const payload = {
-        sub: user.id,
-        username: user.username
-    };
-    const secret = process.env.JWTSECRET
-    const options = {
-      expiresIn: "3h",
-    };
-        return jwt.sign(payload, secret, options);
+  const payload = {
+    sub: user.id,
+    username: user.username,
+  };
+  const secret = process.env.JWTSECRET;
+  const options = {
+    expiresIn: '3h',
+  };
+  return jwt.sign(payload, secret, options);
 }
 
-
-
 //login endpoint for child
-router.post('/:id', authRequired, function (req, res){
-    if(req.body.pin){
+router.post('/:id', authRequired, function (req, res) {
+  if (req.body.pin) {
     //retrieve the parent from the db
     Child.findById(req.params.id)
-      .then((child)=>{
+      .then((child) => {
         //check the pin
-        console.log(child)
-        if(child.pin === req.body.pin){
+        console.log(child);
+        if (child.pin === req.body.pin) {
           //if the pin is correct make a token and get the dashboard data
           const token = createToken(child);
           res.status(200).json({
-              "token": token,
-              "child": {
-                      'id': child.id,
-                      'name': child.name, 
-                      'username': child.username, 
-                      'current_mission': child.current_mission, 
-                      'avatar_url': child.avatar_url  
-                      }
-          })
-        }else{
+            token: token,
+            child: {
+              id: child.id,
+              name: child.name,
+              username: child.username,
+              current_mission: child.current_mission,
+              avatar_url: child.avatar_url,
+            },
+          });
+        } else {
           res.status(400).json({
-            "message": "incorrect pin"
-          })
+            message: 'incorrect pin',
+          });
         }
       })
-      .catch(err=>{
-          res.status(400).json({
-            "message": "there is no parent with that ID",
-            "error": err
-          });
+      .catch((err) => {
+        res.status(400).json({
+          message: 'there is no parent with that ID',
+          error: err,
+        });
       });
-    
-    }else{
-      res.status(400).json({
-        message: 'please include a pin',
-      });
-    }
-})
-
+  } else {
+    res.status(400).json({
+      message: 'please include a pin',
+    });
+  }
+});
 
 //get current mission endpoint
 //check the token
-router.get('/:id/mission', checkToken, function (req,res){
-    if(req.decodedToken.sub == req.params.id){
+router.get('/:id/mission', function (req, res) {
+  if (req.decodedToken.sub == req.params.id) {
     Child.findById(req.params.id)
-        .then((child)=>{
-            Child.getCurrentMission(child.current_mission)
-                .then(mission=>{
-                    res.json({
-                        ...mission
-                    })
-                })
-                .catch(err=>{
-                    res.status(500).json({
-                        "message": "error retrieving mission",
-                        "error": err
-                    })
-                })
-        })
-        .catch(err=>{
+      .then((child) => {
+        Child.getCurrentMission(child.current_mission)
+          .then((mission) => {
+            res.status(200).json({
+              ...mission,
+            });
+          })
+          .catch((err) => {
             res.status(500).json({
-                "message": "error retrieving child data",
-                "error": err
-            })
-        })
-    }else{
-        res.status(400).json({
-            "message": "The ID provided is not associated with the token provided"
-        })
-    }
-})
-
+              message: 'error retrieving mission',
+              error: err,
+            });
+          });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: 'error retrieving child data',
+          error: err,
+        });
+      });
+  } else {
+    res.status(400).json({
+      message: 'The ID provided is not associated with the token provided',
+    });
+  }
+});
 
 //post writting submission
-    //use the multer function to send to the aws bucket and get the url's back
-    //send each of those url's to the ds endpoint to get scores and flags back
-    //add those scores and flags to the urls to make each post object
-    //add each of those post objects to the db
+//use the multer function to send to the aws bucket and get the url's back
+//send each of those url's to the ds endpoint to get scores and flags back
+//add those scores and flags to the urls to make each post object
+//add each of those post objects to the db
 
-router.post('/:id/mission/write', checkToken, async function (req,res){
-
-        child = await Child.findById(req.params.id);
-        //we run the images through this multer function
-        //we send our files to an AWS bucket
-        //we get back an array of urls for the uploaded files
-        multiUpload(req, res, async function(err){
-            //console.log('files', req.files);
-            if(err){
-                return res.status(500).json({
-                    status: 'fail',
-                    message: 'Error: No File Selected'
-                });
-            }else{
-                if(req.files === undefined){
-                    return res.json({'message': 'file undefined'})
-                }
-                else{
-                    const fileArray = req.files
-                    let fileLocation = '';
-                    const images = [];
-                    for(let i = 0; i < fileArray.length; i ++){
-                        fileLocation = fileArray[i].location;
-                        images.push(fileLocation);
-                    };
-                    //this is where the axios calls to ds would be made
-                    //with the url's in the body
-                    //we get the scores and flags back 
-                    //and construct the submission objects to save to the DB
-                    submissions = []
-                    images.map((url)=>{
-                        result = mockDSCall(url);
-                        submissionObject = {
-                            file_path: url,
-                            ...result,
-                            mission_id: child.current_mission,
-                            child_id: child.id
-                        }
-                        submissions.push(submissionObject);
-                        })
-                    console.log(submissions)
-                    //so now we should have an array of objects ready to put in the DB
-                    await submissions.map((obj)=>{
-                         Child.addWriting(obj)
-                            .then(response=>{
-                                console.log(response)
-                            }).catch(err=>{
-                                res.json({
-                                    "error": err
-                                })
-                            })
-                    })
-                    //if an error wasn't thrown that means that we've successfully submitted!
-                    res.status(200).json({
-                        "message": "we got your submission!"
-                    })
-                }
-            }
+router.post('/:id/mission/write', checkToken, async function (req, res) {
+  child = await Child.findById(req.params.id);
+  //we run the images through this multer function
+  //we send our files to an AWS bucket
+  //we get back an array of urls for the uploaded files
+  multiUpload(req, res, async function (err) {
+    //console.log('files', req.files);
+    if (err) {
+      return res.status(500).json({
+        status: 'fail',
+        message: 'Error: No File Selected',
+      });
+    } else {
+      if (req.files === undefined) {
+        return res.json({ message: 'file undefined' });
+      } else {
+        const fileArray = req.files;
+        let fileLocation = '';
+        const images = [];
+        for (let i = 0; i < fileArray.length; i++) {
+          fileLocation = fileArray[i].location;
+          images.push(fileLocation);
+        }
+        //this is where the axios calls to ds would be made
+        //with the url's in the body
+        //we get the scores and flags back
+        //and construct the submission objects to save to the DB
+        submissions = [];
+        images.map((url) => {
+          result = mockDSCall(url);
+          submissionObject = {
+            file_path: url,
+            ...result,
+            mission_id: child.current_mission,
+            child_id: child.id,
+          };
+          submissions.push(submissionObject);
         });
+        console.log(submissions);
+        //so now we should have an array of objects ready to put in the DB
+        await submissions.map((obj) => {
+          Child.addWriting(obj)
+            .then((response) => {
+              console.log(response);
+            })
+            .catch((err) => {
+              res.json({
+                error: err,
+              });
+            });
+        });
+        //if an error wasn't thrown that means that we've successfully submitted!
+        res.status(200).json({
+          message: 'we got your submission!',
+        });
+      }
+    }
+  });
+});
+
+router.post('/:id/mission/draw', checkToken, async function (req, res) {
+  child = await Child.findById(req.params.id);
+
+  singleUpload(req, res, async function (err) {
+    //console.log('files', req.files);
+    if (err) {
+      return res.status(500).json({
+        status: 'fail',
+        message: 'Error: No File Selected',
+      });
+    } else {
+      if (req.file === undefined) {
+        return res.json({ message: 'file undefined' });
+      } else {
+        result = mockDSCall(req.file.location);
+        submissionObject = {
+          file_path: req.file.location,
+          ...result,
+          mission_id: child.current_mission,
+          child_id: child.id,
+        };
+        Child.addWriting(submissionObject)
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((err) => {
+            res.json({ error: err });
+          });
+        res.status(200).json({ message: 'we got your submission!' });
+      }
+    }
+  });
 });
 
 //get past submissions
-router.get('/:id/archive', /*checkToken,*/ function(req, res){
+router.get(
+  '/:id/archive',
+  /*checkToken,*/ function (req, res) {
     Child.getArchive(req.params.id)
-        .then(submissions=>{
-            res.json({submissions});
-        })
-        .catch(err=>{
-            res.json({err})
-        })
-});
+      .then((submissions) => {
+        res.json({ submissions });
+      })
+      .catch((err) => {
+        res.json({ err });
+      });
+  }
+);
 
-
-
-const mockDSCall = function(){
-    return {
-        score: Math.trunc(Math.random() * 100),
-        //flagged: false
-    }
-}
+const mockDSCall = function () {
+  return {
+    score: Math.trunc(Math.random() * 100),
+    //flagged: false
+  };
+};
 
 module.exports = router;
