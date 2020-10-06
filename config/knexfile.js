6f55f0fbd1bd9de402e639eefb02e71502b2112c
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-10-06 10:29:24 -0700

    trying database url to see if gh tests pass

--- config/knexfile.js
+++ config/knexfile.js
@@ -4,19 +4,9 @@ dotenv.config({ path: '../.env' });
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      host: 'localhost',
-      port: 5432,
-      user: 'postgres',
-      database: 'StorySquad',
-      password: 'J3$uspenguin',
-    },
+    connection: process.env.DATABASE_URL,
     migrations: { directory: '../data/migrations' },
     seeds: { directory: '../data/seeds' },
-    pool: {
-      min: 2,
-      max: 10,
-    },
   },
 
   test: {
