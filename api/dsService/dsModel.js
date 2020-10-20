2e84c0f1d488d492682a9ab0a17d66d50d6e34b3
Jack Ross <43938405+JackRossProjects@users.noreply.github.com>
2020-10-20 10:56:38 -0600

    Update dsModel.js

--- api/dsService/dsModel.js
+++ api/dsService/dsModel.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const dsConfig = require('../../config/dsConfig');
 const dsClient = axios.create(dsConfig);
 
-const getPrediction = (x1) => {
-  return dsClient.post('/ocr', { x1 });
+const getPrediction = (URL) => {
+  return dsClient.post('/ocr', { URL });
 };
 
 module.exports = { getPrediction };
