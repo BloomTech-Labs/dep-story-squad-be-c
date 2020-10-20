a304cbd7cd8c9a52550ad7bac441e83a58da68dd
dawsonhammdev <hammsdesigns@gmail.com>
2020-10-20 09:05:42 -0500

    working on parent router tests, having trouble with delete child.

--- __tests__/routes/parent.test.js
+++ __tests__/routes/parent.test.js
@@ -1,8 +1,10 @@
 const request = require('supertest');
 const Parents = require('../../api/parent/parentModel.js');
+const Child = require('../../api/child/childModel');
 const server = require('../../api/app.js');
 
 jest.mock('../../api/parent/parentModel.js');
+jest.mock('../../api/child/childModel.js');
 
 jest.mock('../../api/middleware/authRequired.js', () =>
   jest.fn((req, res, next) => next())
@@ -99,6 +101,122 @@ describe('profiles router endpoints', () => {
 
       expect(res.status).toBe(200);
       expect(res.body.newChild.name).toBe('tim');
+    });
+    it('should return 404 if endpoint is spelled wrong when a new child is created', async () => {
+      Parents.findById.mockResolvedValue({
+        id: '1',
+        pin: '1234',
+        name: 'jeff',
+        email: 'test@testing.com',
+        admin: false,
+      });
+      Parents.createChild.mockResolvedValue({
+        id: 11,
+        name: 'tim',
+        writing_score: 50,
+        current_mission: 1,
+        avatar_url: 'fake/url.com',
+      });
+      const res = await request(server).post('/parent/1/childrens').send({
+        name: 'tim',
+        avatar_url: 'fake/url.com',
+        pin: '4567',
+        username: 'test123',
+        grade: 5,
+      });
+
+      expect(res.status).toBe(404);
+    });
+    it('should return 404 if GET was used instead of POST when a new child is created', async () => {
+      Parents.findById.mockResolvedValue({
+        id: '1',
+        pin: '1234',
+        name: 'jeff',
+        email: 'test@testing.com',
+        admin: false,
+      });
+      Parents.createChild.mockResolvedValue({
+        id: 11,
+        name: 'tim',
+        writing_score: 50,
+        current_mission: 1,
+        avatar_url: 'fake/url.com',
+      });
+      const res = await request(server).get('/parent/1/children').send({
+        name: 'tim',
+        avatar_url: 'fake/url.com',
+        pin: '4567',
+        username: 'test123',
+        grade: 5,
+      });
+
+      expect(res.status).toBe(404);
+    });
+    it('should return 404 if GET was used instead of POST when a new child is created', async () => {
+      Parents.findById.mockResolvedValue({
+        id: '1',
+        pin: '1234',
+        name: 'jeff',
+        email: 'test@testing.com',
+        admin: false,
+      });
+      Parents.createChild.mockResolvedValue({
+        id: 11,
+        name: 'tim',
+        writing_score: 50,
+        current_mission: 1,
+        avatar_url: 'fake/url.com',
+      });
+      const res = await request(server).get('/parent/1/children').send({
+        name: 'tim',
+        avatar_url: 'fake/url.com',
+        pin: '4567',
+        username: 'test123',
+        grade: 5,
+      });
+
+      expect(res.status).toBe(404);
+    });
+    it('should return 404 if PUT was used instead of POST when a new child is created', async () => {
+      Parents.findById.mockResolvedValue({
+        id: '1',
+        pin: '1234',
+        name: 'jeff',
+        email: 'test@testing.com',
+        admin: false,
+      });
+      Parents.createChild.mockResolvedValue({
+        id: 11,
+        name: 'tim',
+        writing_score: 50,
+        current_mission: 1,
+        avatar_url: 'fake/url.com',
+      });
+      const res = await request(server).put('/parent/1/children').send({
+        name: 'tim',
+        avatar_url: 'fake/url.com',
+        pin: '4567',
+        username: 'test123',
+        grade: 5,
+      });
+
+      expect(res.status).toBe(404);
+    });
+  });
+
+  describe('DELETE /parent/:id/children/:child_id', () => {
+    it('should return 200 when child is deleted from DB succesfully', async () => {
+      Child.findById.mockResolvedValue({
+        id: 1,
+        name: 'tim',
+        writing_score: 50,
+        current_Mission: 1,
+        avatar_url: 'fake/url.com',
+        parent_id: 1,
+      });
+      const res = await request(server).delete('/parent/1/children/1');
+      console.log('HERE', res);
+      expect(res.status).toBe(200);
     });
   });
 });
